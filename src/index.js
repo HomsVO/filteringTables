f7afdb416968df6a4e4626287f22e438c4224048
Maksim Vashchenko <33806971+HomsVO@users.noreply.github.com>
2019-04-02 14:30:55 +0300

    fix hashRouter & npm start

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDom.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
   ,
   document.getElementById('root')
 )
